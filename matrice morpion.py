cac5bea39ba78107e3888f9c8fa95c469416b1e1
i182568 <i182568@MPS0919-08>
2020-03-03 15:42:40 +0100

    jouer

--- matrice morpion.py
+++ matrice morpion.py
@@ -1,6 +1,21 @@
 import numpy as np
 a= np.ones((3,3),np.uint8)
 print (a)
-
-
+k=0
+while k<9:
+    #joueur 1 
+    print("le joueur 1 joue !")
+    ligne=input("Choisir une ligne :" )
+    colonne= input("choisir une colonne :")
+    print("Vous avez joué la case ("+ligne+","+colonne+")")
+    a[[int(ligne)],[int(colonne)]]= a[[int(ligne)],[int(colonne)]]+1
+    print (a)
+    #joueur 2
+    print("c'est au tour du 2eme joueur")
+    ligne=input("Choisir une ligne :" )
+    colonne= input("choisir une colonne :")
+    print("Vous avez joué la case ("+ligne+","+colonne+")")
+    a[[int(ligne)],[int(colonne)]]= a[[int(ligne)],[int(colonne)]]-1
+    print (a)
+    k=k+1
 
