a210564f98ef9e6cc1056dd66e584fbf9477ada3
i183879 <i183879@MPS0718-12>
2020-03-09 11:56:46 +0100

    t

--- InterfaceGraph.py
+++ InterfaceGraph.py
@@ -2,34 +2,55 @@
 Interface utilisateur
 """
 
-#import de la bibliothèque wx
-#initialisation de l'interface graphique
-#création de la fenêtre
-#affichage de la fenêtre
-
+import numpy as np
 import wx
+import os
 
-class Menu(wx.Frame):
-    def _init_(self) :
-        super()._init_(parent=None, Title="Jeu du super-morpion")
-        bouton1 = wx.Button(self, id=1000, label='X', pos=(30, 30), size=(50, 20))
-        self.Bind(wx.EVT_BUTTON, self.OnBouton, bouton1)
-    def _installer_menu_(self) :
-        barre_Menu=wx.MenuBar()
-        menu_fichier=wx.Menu
-        article_quitter=menu.fichier.Append(wx.ID_EXIT, 'Quitter', 'Quitter le jeu')
-        barre_menu.Append(menu_fichier, '&fichier')
-        self.SetMenuBar(barre_menu)
-        self.Bind(wx.EVT_Menu, self.OnQuit, article_quitter)
-        
-    def OnQuit(self, e) :
-        print('aticle :', e.GetId() )
-        print("Je quitte")
-            
-        self.Close()
+class MaFenetre(wx.Frame):    
+    def __init__(self, ligne, colonne):
+        chemin_script = os.path.dirname(os.path.realpath(__file__))
+        super().__init__(parent=None, title='Ma fenêtre avec BitmapButton')
+        self.bmp1 = wx.Bitmap("H:\Semestre_4\TraitementImages\Traitement_Image\Collection_images\ceramic.png", wx.BITMAP_TYPE_PNG) 
+        self.bmp2 = wx.Bitmap("H:\Semestre_4\TraitementImages\Traitement_Image\Collection_images\bateau.png", wx.BITMAP_TYPE_PNG) 
+        self.ligne= ligne
+        self.colonne= colonne
+        self.plateau = np.zeros((ligne,colonne),np.int32)
+        nb_boutons = self.plateau.shape[0]*self.plateau.shape[1]
+        self.ma_grille = wx.GridSizer(rows=ligne, cols=colonne, vgap=0, hgap=0)
+        font = wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
+            wx.FONTWEIGHT_BOLD)
+        self.etat_bouton = None
+        for i in range(nb_boutons):
+            bouton = wx.BitmapButton(self, id=i, bitmap=self.bmp1)
+            bouton.SetFont(font)
+            bouton.Bind(wx.EVT_BUTTON, self.OnBouton)            
+            self.ma_grille.Add(bouton, i,wx.EXPAND)
+            if self.etat_bouton == None:
+                self.etat_bouton={i: False}
+            else:
+                self.etat_bouton[i] = False
+        self.SetSizerAndFit(self.ma_grille)        
+        self.Show()
 
-if __name__ == '__main__' :
+    def OnBouton(self, event):
+        if event.GetId() in self.etat_bouton:
+            bouton = event.GetEventObject()
+            print(event.GetId())
+            if not self.etat_bouton[event.GetId()]:
+                self.etat_bouton[event.GetId()] = True
+                bouton.SetBitmapLabel(self.bmp2)
+            else:
+                self.etat_bouton[event.GetId()] = False
+                bouton.SetBitmapLabel(self.bmp1)
+
+        ligne = event.GetId() // self.colonne
+        colonne = event.GetId() % self.colonne
+
+     
+        print(ligne," ",colonne)
+       
+if __name__ == '__main__':
     app = wx.App()
-    frame = wx.Frame(parent=None, title="Jeu du super-morpion")
-    frame.Show()
+    frame = MaFenetre(9,9)
     app.MainLoop()
+
