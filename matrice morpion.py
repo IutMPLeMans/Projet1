c9fbd3951b42a7db1623b51cd856e8541e4501b7
i182568 <i182568@MPS0919-08>
2020-03-10 15:29:57 +0100

    sal

--- matrice morpion.py
+++ matrice morpion.py
@@ -8,61 +8,79 @@ class Plateau:
     def __repr__(self):
         return np.array_str(self.p)
     def caselibre(self,l,c):
-        if  self.p[[int(l)],[int(c)]]==0 or l>ligne or c>colonne:
-            condition= True
-        else:
-            print(" cette case est impossible à selectionner veuillez en choisir une autre")
+        condition= False
+        if  self.p[l][c]==0 or l>ligne or c>colonne:
+            return True
 
-def gagne(grille):
-    if (a[0,:].all() or a[1,:].all() or a[2,:].all() or a[:,0].all() or a[:,1].all() or a[:,2].all())==1    or   a[0,0]==a[1,1]==a[2,2]    or   a[0,2]==a[1,1]==a[2,0]:
+        return False
+
+    def set1(self,l,c):
+        self.p[l][c]=="X"
+    def set2(self,l,c):
+        self.p[l][c]=="O"
+
+    def est_gagnant(self):
+        if (self.p[0]==self.p[1]) and (self.p[0]==grille[2]) and (self.p[0]==("X" or "O")):
+            return 1
+        if (self.p[3]==self.p[4]) and (self.p[3]==self.p[5]) and (self.p[3]==("X" or "O")):
+            return 1
+        if (self.p[6]==self.p[7]) and (self.p[6]==self.p[8]) and (self.p[6]==("X" or "O")):
+            return 1
+        if (self.p[0]==self.p[3]) and (self.p[0]==self.p[6]) and (self.p[0]==("X" or "O")):
+            return 1
+        if (self.p[1]==self.p[4]) and (self.p[1]==self.pe[7]) and (self.p[1]==("X" or "O")):
+            return 1
+        if (self.p[2]==self.p[5]) and (self.p[2]==self.p[8]) and (self.p[2]==("X" or "O")):
+            return 1
+        if (self.p[0]==self.p[4]) and (self.p[0]==self.p[8]) and (self.p[0]==("X" or "O")):
+            return 1
+        if (self.p[2]==self.p[4]) and (self.p[2]==self.p[6]) and (self.p[2]==("X" or "O")):
+            return 1
+
+
+    def nul(self):
+        for i in range(9):
+            if self.p[i]==1:
+                return 0
         return 1
 
-def nul(grille):
-    for i in range(9):
-        if grille[i]==1:
-            return 0
-    return 1
-
-def jeu(grille):
-    condition= False
-    
-    tableau=Plateau(3,3)
-    k=0
-    while condition== False:
-        k=k+1
-        if k%2==0:
-            print("C'est au tour du joueur 2")
-            print(tableau)
-            ligne=input("Choisir une ligne: ")
-            colonne=input("Choisir une colonne: ")
-            if :
-                condition= True
-                tableau[[int(ligne)],[int(colonne)]]=tableau[int(ligne)],[int(colonne)]+1
+    def jeu(self):
+        condition= False
+        jeu=Plateau(9,9)
+        k=0
+        while condition== False:
+            k=k+1
+            if k%2==0:
+                print("C'est au tour du joueur 2")
+                print(jeu)
+                ligne=int(input("Choisir une ligne: "))
+                colonne=int(input("Choisir une colonne: "))
+                if jeu.caselibre(l,c) :
+                    jeu.set1(l,c)
+                else:
+                    print(" cette case est impossible à selectionner veuillez en choisir une autre")
             else:
-                print(" cette case est impossible à selectionner veuillez en choisir une autre")
-        else:
-            print("C'est au tour du joueur 1")
-            print(tableau)
-            ligne=input("Choisir une ligne: ")
-            colonne=input("Choisir une colonne: ")
-            if tableau[[int(ligne)],[int(colonne)]]==0 or ligne>3 or colonne>3:
-                condition= True
-                tableau[[int(ligne)],[int(colonne)]]=tableau[int(ligne)],[int(colonne)]-1
-            else:
-                print(" cette case est impossible à selectionner veuillez en choisir une autre")
-    condition=False
-    print(tableau)
+                print("C'est au tour du joueur 1")
+                print(jeu)
+                ligne=input("Choisir une ligne: ")
+                colonne=input("Choisir une colonne: ")
+                if jeu.caselibre(l,c) :
+                    jeu.set2(l,c)
+                else:
+                    print(" cette case est impossible à selectionner veuillez en choisir une autre")
+        condition=False
+        print(jeu)
 
 gagne= False
 while gagne==False:
-    jeu(grille)
-    if nul(grille):
+    jeu(self)
+    if nul(self):
         print("Matche Nul")
         gagne=True
-    elif gagne(grille):
-        if 0==gagne(grille):
+    elif gagne(self):
+        if 0==gagne(self):
             print("le joueur 1 à gagné!")
-        elif 1==gagne(grille):
+        elif 1==gagne(slef):
             print("le joueurs 2 à gagné!")
     gange=True
     
